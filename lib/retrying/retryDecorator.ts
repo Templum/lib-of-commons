3f2d59733c52c12509482ed73f415fd38bb89b64
Simon Pelczer <templum.dev@gmail.com>
2019-04-11 22:09:15 +0200

    :sparkles: Added Retry Decorator (Sync/Async)

---
+++ lib/retrying/retryDecorator.ts
@@ -0,0 +1,39 @@
+export function AsyncRetry(isRetrieable: (error: Error) => boolean, times: number = 3) {
+    return function (
+        target: any,
+        propertyName: string,
+        propertyDesciptor: PropertyDescriptor): PropertyDescriptor {
+        const method = propertyDesciptor.value;
+
+        propertyDesciptor.value = async function (...args: any[]) {
+            for (let attempt = 0; attempt < times; attempt++) {
+                try {
+                    return await method.apply(this, args);
+                } catch (error) {
+                    if (!isRetrieable(error) || attempt + 1 >= times) { throw error; }
+                }
+            }
+        };
+        return propertyDesciptor;
+    };
+}
+
+export function Retry(isRetrieable: (error: Error) => boolean, times: number = 3) {
+    return function (
+        target: any,
+        propertyName: string,
+        propertyDesciptor: PropertyDescriptor): PropertyDescriptor {
+        const method = propertyDesciptor.value;
+
+        propertyDesciptor.value = function (...args: any[]) {
+            for (let attempt = 0; attempt < times; attempt++) {
+                try {
+                    return method.apply(this, args);
+                } catch (error) {
+                    if (!isRetrieable(error) || attempt + 1 >= times) { throw error; }
+                }
+            }
+        };
+        return propertyDesciptor;
+    };
+}
