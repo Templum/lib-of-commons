e197200ead08a3c28a29732774e07b852feabe80
Simon Pelczer <templum.dev@gmail.com>
2019-04-11 22:09:35 +0200

    :white_check_mark: Started Testing Retry Decorator

---
+++ __tests__/retrying/retryDecorator.test.ts
@@ -0,0 +1,137 @@
+import { Retry } from "../../lib";
+
+function isRetrieableError(error: Error) { return error.message === 'Expected'; }
+
+describe('Retry Decorator', () => {
+    describe('Contract', () => {
+        it('should retry the function if it fails with an retriable error', () => {
+            class Example {
+                public spy: () => boolean;
+
+                constructor() {
+                    const mock = jest
+                        .fn()
+                        .mockImplementationOnce(() => { throw new Error('Expected') })
+                        .mockImplementationOnce(() => true);
+                    this.spy = mock;
+                }
+
+                @Retry(isRetrieableError)
+                network() {
+                    return this.spy()
+                }
+            }
+
+            const instance = new Example();
+
+            const result = instance.network();
+            expect(result).toEqual(true);
+            expect(instance.spy).toBeCalledTimes(2);
+        });
+
+        it('should retry the function multiple times', () => {
+            class Example {
+                public spy: () => boolean;
+
+                constructor() {
+                    const mock = jest
+                        .fn()
+                        .mockImplementationOnce(() => { throw new Error('Expected') })
+                        .mockImplementationOnce(() => { throw new Error('Expected') })
+                        .mockImplementationOnce(() => { throw new Error('Expected') })
+                        .mockImplementationOnce(() => true);
+                    this.spy = mock;
+                }
+
+                @Retry(isRetrieableError, 4)
+                network() {
+                    return this.spy()
+                }
+            }
+
+            const instance = new Example();
+
+            const result = instance.network();
+            expect(result).toEqual(true);
+            expect(instance.spy).toBeCalledTimes(4);
+        });
+
+        it('should retry at most n times before failing', () => {
+            class Example {
+                public spy: () => boolean;
+
+                constructor() {
+                    const mock = jest
+                        .fn()
+                        .mockImplementation(() => { throw new Error('Expected') })
+                    this.spy = mock;
+                }
+
+                @Retry(isRetrieableError, 40)
+                public network(): boolean {
+                    return this.spy()
+                }
+            }
+
+            const instance = new Example();
+
+            expect(instance.network.bind(instance)).toThrowError('Expected');
+            expect(instance.spy).toBeCalledTimes(40);
+        });
+
+        it('should not retry the function if the error is not retriable', () => {
+            class Example {
+                public spy: () => boolean;
+
+                constructor() {
+                    const mock = jest
+                        .fn()
+                        .mockImplementation(() => { throw new Error('Unexpected') })
+                    this.spy = mock;
+                }
+
+                @Retry(isRetrieableError)
+                public network(): boolean {
+                    return this.spy()
+                }
+            }
+
+            const instance = new Example();
+
+            expect(instance.network.bind(instance)).toThrowError('Unexpected');
+            expect(instance.spy).toBeCalledTimes(1);
+        });
+    });
+
+    describe('Special Cases', () => {
+        it('should not allow 0 as parameter for times', () => {
+            class Example {
+                public spy: () => boolean;
+
+                constructor() {
+                    const mock = jest
+                        .fn()
+                        .mockImplementationOnce(() => { throw new Error('Expected') })
+                        .mockImplementationOnce(() => true);
+                    this.spy = mock;
+                }
+
+                @Retry(isRetrieableError, 0)
+                network() {
+                    return this.spy()
+                }
+            }
+
+            const instance = new Example();
+
+            expect(instance.network).toThrowError();
+            expect(instance.spy).toBeCalledTimes(0);
+        });
+    });
+});
+
+describe('Async Retry Decorator', () => {
+    describe('Contract', () => {
+
+    });
+});
