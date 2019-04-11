65d715a8395616a3640755c9302a7a5f7e416d3a
Simon Pelczer <templum.dev@gmail.com>
2019-04-11 22:10:00 +0200

    :sparkles: Exposing Retry Decorators

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export { ICache } from "./caching/cache";
 export { Cache, CacheKey } from "./caching/cacheDecorator";
 export { Hide } from "./logging/hideParamDecorator";
 export { IToolboxLogger, Level, Log } from "./logging/logDecorator";
+export { AsyncRetry, Retry } from "./retrying/retryDecorator";
