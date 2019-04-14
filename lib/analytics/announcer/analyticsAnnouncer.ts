1b9f82f96b05e1273a46e42328e54ae5151d7071
Simon Pelczer <templum.dev@gmail.com>
2019-04-14 19:58:42 +0200

    :sparkles: Added Domain Probe Concept in form of an announcer

---
+++ lib/analytics/announcer/analyticsAnnouncer.ts
@@ -0,0 +1,64 @@
+import { EventEmitter } from "events";
+
+const ANALYTICS = Symbol('Analytics');
+
+// tslint:disable-next-line: interface-name
+export interface Announcement<Data> {
+    key: string;
+    kind: AnnouncementType;
+    data: Data;
+    timestamp: number;
+}
+
+export enum AnnouncementType {
+    Time = 'executionTime',
+    Metric = 'metric',
+    Event = 'event',
+}
+
+export interface IMonitor<Data> {
+    getIdentifier(): string;
+    handAnnouncement(announcement: Announcement<Data>): void;
+}
+
+export interface IAnnouncer {
+    register(monitor: IMonitor<any>): void;
+    unregister(monitor: IMonitor<any>): void;
+}
+
+export interface IAnnouncerInternal extends IAnnouncer {
+    announce(data: Announcement<any>): void;
+}
+
+export class AnalyticsAnnouncer implements IAnnouncerInternal {
+    private subscribers: Array<IMonitor<any>>;
+    private eventBus: EventEmitter;
+
+    constructor() {
+        this.subscribers = [];
+        this.eventBus = new EventEmitter();
+    }
+
+    public announce(data: Announcement<any>): void {
+        if (this.eventBus.listenerCount(ANALYTICS) === 0) {
+            console.info('No Monitor found, will drop data');
+            return;
+        }
+        console.info(`Announced Data from ${data.key} created at ${data.timestamp}`);
+        this.eventBus.emit(ANALYTICS, data);
+    }
+
+    public register(monitor: IMonitor<any>): void {
+        this.subscribers.push(monitor);
+        this.eventBus.addListener(ANALYTICS, monitor.handAnnouncement);
+        console.info(`Registered Monitor ${monitor.getIdentifier()}`);
+    }
+
+    public unregister(monitor: IMonitor<any>): void {
+        const idx = this.subscribers.findIndex((current) => current.getIdentifier() === monitor.getIdentifier());
+        this.subscribers.splice(idx, 1);
+        this.eventBus.removeListener(ANALYTICS, monitor.handAnnouncement);
+
+        console.info(`Removed Monitor ${monitor.getIdentifier()}`);
+    }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,23 @@
-export { ICache } from "./caching/cache";
+import { AnalyticsAnnouncer, IAnnouncer } from "./analytics/announcer/analyticsAnnouncer";
+
+export { IAnnouncer, IAnnouncerInternal, IMonitor } from "./analytics/announcer/analyticsAnnouncer";
+export { ICache, ICacheEntry } from "./caching/cache";
 export { Cache, CacheKey } from "./caching/cacheDecorator";
 export { Hide } from "./logging/hideParamDecorator";
 export { IToolboxLogger, Level, Log } from "./logging/logDecorator";
 export { AsyncRetry, Retry } from "./retrying/retryDecorator";
+
+class LibOfCommons {
+    private announcerInstance: IAnnouncer | undefined;
+
+    constructor() { }
+
+    public getAnnouncerInstance(): IAnnouncer {
+        if (!this.announcerInstance) {
+            this.announcerInstance = new AnalyticsAnnouncer();
+        }
+        return this.announcerInstance;
+    }
+}
+
+export const libOfCommons = new LibOfCommons();
