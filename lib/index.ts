c19a1158a3ca92231dc205499a51fc711fcc5709
Simon Pelczer <templum.dev@gmail.com>
2019-07-13 15:20:12 +0200

    :truck: Moved Debounce + Retry into package execution

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,18 @@
 import { AnalyticsAnnouncer } from './analytics/announcer/analyticsAnnouncer';
 import { IAnnouncer, InternalAnnouncer } from './analytics/announcer/IAnnouncer';
 
-// tslint:disable-next-line: max-line-length
 export { AnalyticsAnnouncer } from './analytics/announcer/analyticsAnnouncer';
 export { Announcement, AnnouncementType } from './analytics/announcer/announcement';
 export { Counter } from './analytics/counter';
-export { IMonitor } from './analytics/monitor/IMonitor';
 export { Measure, TimeUnit } from './analytics/measure';
+export { IMonitor } from './analytics/monitor/IMonitor';
 export { Cache, CacheKey } from './caching/cacheDecorator';
 export { ICache, ICacheEntry } from './caching/ICache';
+export { Debounce } from './execution/debounceDecorator';
+export { AsyncRetry, Retry } from './execution/retryDecorator';
 export { Hide } from './logging/hideParamDecorator';
 export { IToolboxLogger } from './logging/IToolboxLogger';
 export { Level, Log } from './logging/logDecorator';
-export { Debounce } from './retrying/debounceDecorator';
-export { AsyncRetry, Retry } from './retrying/retryDecorator';
 export { isGreater } from './validation/isGreater';
 export { IsGreaterOrEqual } from './validation/isGreaterOrEqual';
 export { isInteger } from './validation/isInteger';
